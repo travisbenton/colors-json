3265979db8874095c1c02b9c607a6ed4ea1a6915
Travis <travisryanbenton@gmail.com>
2021-03-26 15:15:30 -0500

    error message

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ const disclaimer = `
 `
 const template = (fn, join) => Object.keys(COLORS).map(fn).join(join || '')
 const lessTemplate = `${disclaimer}${template(color => `@${color}: #${COLORS[color]};\n`)}`
-const jsTemplate = `${disclaimer}${template(color => `const ${color} = '#${COLORS[color]}'`, '\n')}\n\nexport default {\n  ${template(color => `${color}`, ',\n\t')}\n}`
+const jsTemplate = `${disclaimer}${template(color => `const ${color} = '#${COLORS[color]}'`, '\n')}\n\nexport default {\n\xa0\xa0${template(color => `${color}`, ',\n\xa0\xa0')}\n}`
 
 // regex to weed out any non-hex color values in colors.json file
 const badColors = Object.keys(COLORS).filter(key => !COLORS[key].match(/^[0-9a-f]{3,6}$/i))
 
 if (badColors.length) {
-  console.log('ERROR: INVALID HEX CODE FOUND. PLEASE CHECK THE FOLLOWING KEYS');
+  console.log('ERROR: INVALID HEX CODE FOUND. PLEASE CHECK THE FOLLOWING DEFINITIONS');
   console.log(badColors.map(key => `"${key}": "${COLORS[key]}"`).join(', '));
 } else {
   fs.writeFile('./colors/colors.less', lessTemplate, () => console.log('.less file compiled succesfully'))
