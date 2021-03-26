5f8933724c9a4021c09f91200dc98f4e168a7f9a
Travis <travisryanbenton@gmail.com>
2021-03-26 15:10:05 -0500

    cleanup code and add error

--- colors/colors.js
+++ colors/colors.js
@@ -1,13 +1,125 @@
 
-
 // ============================================
 // WARNING: This file has been auto-generated
 // ============================================
+const BLUE1= '#0e6eb4'
+const BLUE2= '#00a1df'
+const BLUE3= '#49b3e6'
+const BLUE4= '#82c5ec'
+const BLUE5= '#afd8f2'
+const BLUE6= '#d7ebf8'
+const BLUE7= '#eaf4fb'
+const BLUE8= '#eff8ff'
+const GREY1= '#5b6670'
+const GREY2= '#97a3ae'
+const GREY3= '#abb5bd'
+const GREY4= '#bfc7cd'
+const GREY5= '#d4d9dd'
+const GREY6= '#e9ebed'
+const GREY7= '#f3f4f5'
+const GREY8= '#f5f6fa'
+const BLACK1= '#171b1f'
+const BLACK2= '#191d22'
+const BLACK3= '#1e2328'
+const BLACK4= '#262c33'
+const BLACK5= '#323b44'
+const BLACK6= '#424d59'
+const BLACK7= '#515f6e'
+const BLACK8= '#5d6a79'
+const GREEN1= '#48a82a'
+const GREEN2= '#6abf4f'
+const GREEN3= '#91d37b'
+const GREEN4= '#b2e1a1'
+const GREEN5= '#c9eabc'
+const GREEN6= '#d7f0cc'
+const GREEN7= '#def2d4'
+const GREEN8= '#e3f7d9'
+const RED1= '#d60e00'
+const RED2= '#e32b1e'
+const RED3= '#ee6258'
+const RED4= '#f69088'
+const RED5= '#fbafa9'
+const RED6= '#fec1bd'
+const RED7= '#ffcbc7'
+const RED8= '#ffd5d1'
+const ORANGE1= '#f56f10'
+const ORANGE2= '#f88d3d'
+const ORANGE3= '#fbab6d'
+const ORANGE4= '#fdc293'
+const ORANGE5= '#fed0ac'
+const ORANGE6= '#ffd9ba'
+const ORANGE7= '#ffddc2'
+const ORANGE8= '#ffe4ce'
+const YELLOW1= '#ffd500'
+const YELLOW2= '#fed921'
+const YELLOW3= '#fee360'
+const YELLOW4= '#fdeb92'
+const YELLOW5= '#fdf0b1'
+const YELLOW6= '#fdf3c4'
+const YELLOW7= '#fcf5cd'
+const YELLOW8= '#fef8d9'
+const OFF_WHITE= '#fafafa'
 const WHITE= '#ffffff'
 const BLACK= '#000000'
 
 export default {
+  BLUE1,
+	BLUE2,
+	BLUE3,
+	BLUE4,
+	BLUE5,
+	BLUE6,
+	BLUE7,
+	BLUE8,
+	GREY1,
+	GREY2,
+	GREY3,
+	GREY4,
+	GREY5,
+	GREY6,
+	GREY7,
+	GREY8,
+	BLACK1,
+	BLACK2,
+	BLACK3,
+	BLACK4,
+	BLACK5,
+	BLACK6,
+	BLACK7,
+	BLACK8,
+	GREEN1,
+	GREEN2,
+	GREEN3,
+	GREEN4,
+	GREEN5,
+	GREEN6,
+	GREEN7,
+	GREEN8,
+	RED1,
+	RED2,
+	RED3,
+	RED4,
+	RED5,
+	RED6,
+	RED7,
+	RED8,
+	ORANGE1,
+	ORANGE2,
+	ORANGE3,
+	ORANGE4,
+	ORANGE5,
+	ORANGE6,
+	ORANGE7,
+	ORANGE8,
+	YELLOW1,
+	YELLOW2,
+	YELLOW3,
+	YELLOW4,
+	YELLOW5,
+	YELLOW6,
+	YELLOW7,
+	YELLOW8,
+	OFF_WHITE,
 	WHITE,
 	BLACK
 }
-	
--- index.js
+++ index.js
@@ -6,26 +6,17 @@ const disclaimer = `
 // WARNING: This file has been auto-generated
 // ============================================\r\r\r
 `
-
 const template = (fn, join) => Object.keys(COLORS).map(fn).join(join || '')
+const lessTemplate = `${disclaimer}${template(color => `@${color}: #${COLORS[color]};\n`)}`
+const jsTemplate = `${disclaimer}${template(color => `const ${color}= '#${COLORS[color]}'`, '\n')}\n\nexport default {\n  ${template(color => `${color}`, ',\n\t')}\n}`
 
-fs.writeFile(
-	'./colors/colors.less',
+// regex to weed out any non-hex color values in colors.json file
+const badColors = Object.keys(COLORS).filter(key => !COLORS[key].match(/^[0-9a-f]{3,6}$/i))
 
-	`
-${disclaimer}${template(color => `@${color}: ${COLORS[color]};\n`)}`,
-
-	() => console.log('LESS VARIABLES DONE')
-)
-
-fs.writeFile(
-	'./colors/colors.js',
-	`
-${disclaimer}${template(color => `const ${color}= '${COLORS[color]}'`, '\n')}
-
-export default {
-	${template(color => `${color}`, ',\n\t')}
+if (badColors.length) {
+  console.log('ERROR: INVALID HEX CODE FOUND. PLEASE CHECK THE FOLLOWING KEYS');
+  console.log(badColors.map(key => `"${key}": "${COLORS[key]}"`).join(', '));
+} else {
+  fs.writeFile('./colors/colors.less', lessTemplate, () => console.log('.less file compiled succesfully'))
+  fs.writeFile('./colors/colors.js', jsTemplate, () => console.log('.js file compiled succesfully'))
 }
-	`,
-	() => console.log('JS VARIABLES DONE')
-)
